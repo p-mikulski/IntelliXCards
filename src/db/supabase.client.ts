3f4a03b3c513cd4339329421d5ab706225f47c4f
p-mikulski <piotr.mikulski.imptob@gmail.com>
2025-10-23 07:35:37 +0200

    Update DEFAULT_USER_ID in Supabase client configuration

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -1,11 +1,8 @@
-/* eslint-disable prettier/prettier */
 import { createClient } from "@supabase/supabase-js";
-
 import type { Database } from "../db/database.types.ts";
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
-
-export const DEFAULT_USER_ID = "00000000-0000-0000-0000-000000000000";
+export const DEFAULT_USER_ID = "41f39613-d7fb-4a10-8a7e-4646b56b2dba";
